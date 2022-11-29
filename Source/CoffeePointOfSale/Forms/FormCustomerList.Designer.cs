a26d955cb6ba7e56d2e9415b755ed2172660002b
Habeebah157 <63429228+Habeebah157@users.noreply.github.com>
2022-11-29 18:08:40 -0500

    Edit display

--- Source/CoffeePointOfSale/Forms/FormCustomerList.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormCustomerList.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.firstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.lastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnOrder = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -51,36 +53,13 @@
             this.firstName,
             this.lastName,
             this.btnOrder});
-            this.dataGridView1.Location = new System.Drawing.Point(241, 74);
+            this.dataGridView1.Location = new System.Drawing.Point(368, 109);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(866, 427);
+            this.dataGridView1.Size = new System.Drawing.Size(606, 256);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(189, 34);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Return to Main Menu";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(565, 545);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(218, 40);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Add New Customer";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // customersBindingSource
-            // 
-            this.customersBindingSource.DataSource = typeof(CoffeePointOfSale.Services.Customer.Customers);
             // 
             // phone
             // 
@@ -113,19 +92,59 @@
             this.btnOrder.Name = "btnOrder";
             this.btnOrder.Width = 150;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(30, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Return to Main Menu";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(95)))), ((int)(((byte)(98)))));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(587, 418);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add New Customer";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // customersBindingSource
+            // 
+            this.customersBindingSource.DataSource = typeof(CoffeePointOfSale.Services.Customer.Customers);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(-10, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1366, 62);
+            this.panel1.TabIndex = 3;
+            // 
             // FormCustomerList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1326, 665);
+            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(13, 22, 13, 22);
+            this.Margin = new System.Windows.Forms.Padding(9, 13, 9, 13);
             this.Name = "FormCustomerList";
             this.Text = "HIDE Coffee";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -140,5 +159,6 @@
         private DataGridViewTextBoxColumn firstName;
         private DataGridViewTextBoxColumn lastName;
         private DataGridViewButtonColumn btnOrder;
+        private Panel panel1;
     }
 }
--- Source/CoffeePointOfSale/Forms/FormMain.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormMain.Designer.cs
@@ -31,46 +31,80 @@
             this.btnCreateOrder = new System.Windows.Forms.Button();
             this.btnCustomerList = new System.Windows.Forms.Button();
             this.btnManagement = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.mainmenu = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCreateOrder
             // 
-            this.btnCreateOrder.Location = new System.Drawing.Point(214, 67);
-            this.btnCreateOrder.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCreateOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(166)))), ((int)(((byte)(255)))));
+            this.btnCreateOrder.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCreateOrder.ForeColor = System.Drawing.Color.White;
+            this.btnCreateOrder.Location = new System.Drawing.Point(491, 171);
+            this.btnCreateOrder.Margin = new System.Windows.Forms.Padding(2);
             this.btnCreateOrder.Name = "btnCreateOrder";
-            this.btnCreateOrder.Size = new System.Drawing.Size(122, 20);
+            this.btnCreateOrder.Size = new System.Drawing.Size(350, 85);
             this.btnCreateOrder.TabIndex = 0;
             this.btnCreateOrder.Text = "Create Order";
-            this.btnCreateOrder.UseVisualStyleBackColor = true;
+            this.btnCreateOrder.UseVisualStyleBackColor = false;
             this.btnCreateOrder.Click += new System.EventHandler(this.btnCreateOrder_Click);
             // 
             // btnCustomerList
             // 
-            this.btnCustomerList.Location = new System.Drawing.Point(221, 128);
-            this.btnCustomerList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCustomerList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(211)))), ((int)(((byte)(83)))));
+            this.btnCustomerList.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCustomerList.ForeColor = System.Drawing.Color.White;
+            this.btnCustomerList.Location = new System.Drawing.Point(498, 305);
+            this.btnCustomerList.Margin = new System.Windows.Forms.Padding(2);
             this.btnCustomerList.Name = "btnCustomerList";
-            this.btnCustomerList.Size = new System.Drawing.Size(115, 20);
+            this.btnCustomerList.Size = new System.Drawing.Size(343, 85);
             this.btnCustomerList.TabIndex = 1;
             this.btnCustomerList.Text = "Customer List";
-            this.btnCustomerList.UseVisualStyleBackColor = true;
+            this.btnCustomerList.UseVisualStyleBackColor = false;
             this.btnCustomerList.Click += new System.EventHandler(this.btnCustomerList_Click);
             // 
             // btnManagement
             // 
-            this.btnManagement.Location = new System.Drawing.Point(221, 184);
-            this.btnManagement.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnManagement.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(95)))), ((int)(((byte)(98)))));
+            this.btnManagement.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnManagement.ForeColor = System.Drawing.Color.White;
+            this.btnManagement.Location = new System.Drawing.Point(532, 442);
+            this.btnManagement.Margin = new System.Windows.Forms.Padding(2);
             this.btnManagement.Name = "btnManagement";
-            this.btnManagement.Size = new System.Drawing.Size(118, 20);
+            this.btnManagement.Size = new System.Drawing.Size(275, 51);
             this.btnManagement.TabIndex = 2;
-            this.btnManagement.Text = "Management";
-            this.btnManagement.UseVisualStyleBackColor = true;
+            this.btnManagement.Text = "Management Tools";
+            this.btnManagement.UseVisualStyleBackColor = false;
             this.btnManagement.Click += new System.EventHandler(this.btnManagement_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.mainmenu);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1368, 82);
+            this.panel1.TabIndex = 3;
+            // 
+            // mainmenu
+            // 
+            this.mainmenu.AutoSize = true;
+            this.mainmenu.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mainmenu.ForeColor = System.Drawing.Color.White;
+            this.mainmenu.Location = new System.Drawing.Point(562, 10);
+            this.mainmenu.Name = "mainmenu";
+            this.mainmenu.Size = new System.Drawing.Size(226, 54);
+            this.mainmenu.TabIndex = 0;
+            this.mainmenu.Text = "Main Menu";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 270);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(151)))), ((int)(((byte)(126)))));
+            this.ClientSize = new System.Drawing.Size(1364, 760);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnManagement);
             this.Controls.Add(this.btnCustomerList);
             this.Controls.Add(this.btnCreateOrder);
@@ -78,6 +112,8 @@
             this.Name = "FormMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -87,5 +123,7 @@
         private Button btnCreateOrder;
         private Button btnCustomerList;
         private Button btnManagement;
+        private Panel panel1;
+        private Label mainmenu;
     }
 }
