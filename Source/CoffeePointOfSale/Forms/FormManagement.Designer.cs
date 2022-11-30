67db344054414d0a14819e39ca9e25cd9cfeeb39
EvanFinelli <114954136+EvanFinelli@users.noreply.github.com>
2022-11-30 14:15:05 -0500

    Works!!!

--- Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
@@ -29,59 +29,48 @@
         private void InitializeComponent()
         {
             this.btnClose = new System.Windows.Forms.Button();
-            this.txtDeleteThis = new System.Windows.Forms.TextBox();
-            this.lblRemoveThis = new System.Windows.Forms.Label();
+            this.BtnGenCsv = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(187, 35);
+            this.btnClose.Location = new System.Drawing.Point(164, 26);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(94, 29);
+            this.btnClose.Size = new System.Drawing.Size(82, 22);
             this.btnClose.TabIndex = 0;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.OnClickBtnClose);
             // 
-            // txtDeleteThis
+            // BtnGenCsv
             // 
-            this.txtDeleteThis.AcceptsReturn = true;
-            this.txtDeleteThis.Location = new System.Drawing.Point(187, 109);
-            this.txtDeleteThis.Multiline = true;
-            this.txtDeleteThis.Name = "txtDeleteThis";
-            this.txtDeleteThis.Size = new System.Drawing.Size(550, 100);
-            this.txtDeleteThis.TabIndex = 1;
-            // 
-            // lblRemoveThis
-            // 
-            this.lblRemoveThis.AutoSize = true;
-            this.lblRemoveThis.Location = new System.Drawing.Point(187, 87);
-            this.lblRemoveThis.Name = "lblRemoveThis";
-            this.lblRemoveThis.Size = new System.Drawing.Size(177, 20);
-            this.lblRemoveThis.TabIndex = 2;
-            this.lblRemoveThis.Text = "Demo - Remove This Box";
+            this.BtnGenCsv.Location = new System.Drawing.Point(499, 198);
+            this.BtnGenCsv.Name = "BtnGenCsv";
+            this.BtnGenCsv.Size = new System.Drawing.Size(136, 85);
+            this.BtnGenCsv.TabIndex = 1;
+            this.BtnGenCsv.Text = "Generate CSV file and open in Excel";
+            this.BtnGenCsv.UseVisualStyleBackColor = true;
+            this.BtnGenCsv.Click += new System.EventHandler(this.BtnGenCsv_Click);
             // 
             // FormManagement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1346, 721);
-            this.Controls.Add(this.lblRemoveThis);
-            this.Controls.Add(this.txtDeleteThis);
+            this.ClientSize = new System.Drawing.Size(1178, 541);
+            this.Controls.Add(this.BtnGenCsv);
             this.Controls.Add(this.btnClose);
-            this.Margin = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormManagement";
             this.Text = "FormManagement";
             this.Load += new System.EventHandler(this.OnLoadFormManagement);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button btnClose;
-        private TextBox txtDeleteThis;
-        private Label lblRemoveThis;
+        private Button BtnGenCsv;
     }
 }
