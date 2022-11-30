373cb3d8ecc1963f514bea37c242c7bd5ad270d8
duncanstephan <114888030+duncanstephan@users.noreply.github.com>
2022-11-30 00:49:42 -0500

    created FormError

---
+++ Source/CoffeePointOfSale/Forms/FormError.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CoffeePointOfSale.Forms
+{
+    partial class FormError
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(12, 291);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(776, 147);
+            this.btnConfirm.TabIndex = 0;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // txtError
+            // 
+            this.txtError.Location = new System.Drawing.Point(12, 128);
+            this.txtError.Name = "txtError";
+            this.txtError.Size = new System.Drawing.Size(776, 31);
+            this.txtError.TabIndex = 1;
+            // 
+            // FormError
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.btnConfirm);
+            this.Name = "FormError";
+            this.Text = "FormError";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnConfirm;
+        private TextBox txtError;
+    }
+}
