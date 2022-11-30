7275026b771b1611eb8abc6a170311775b92acf3
EvanFinelli <114954136+EvanFinelli@users.noreply.github.com>
2022-11-29 22:07:55 -0500

    update??

--- Source/CoffeePointOfSale/Services/CsvExtract/ICsvExtract.cs
+++ Source/CoffeePointOfSale/Services/CsvExtract/ICsvExtract.cs
@@ -1,4 +1,10 @@
-﻿namespace CoffeePointOfSale.Services.CsvExtract
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeePointOfSale.Services.CsvExtract
 {
     public interface ICsvExtract
     {
